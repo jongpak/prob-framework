2b2b6d9b6b95c21cf82632c4803a3bcae6ece168
jongpak <jplabs@canxan.com>
2016-08-17 00:24:34 +0900

    ViewModel 클래스 테스트 코드 추가

---
+++ tests/ViewModelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+use PHPUnit\Framework\TestCase;
+
+class ViewModelTest extends TestCase
+{
+
+    public function testSetVariable()
+    {
+        $viewModel = new ViewModel();
+        $viewModel->set('key1', 'one');
+        $viewModel->set('key2', 'two');
+        $viewModel->set('key3', 3);
+        $viewModel->set('key4', null);
+        $viewModel->set('key5', ['test', 'ok']);
+
+        $this->assertEquals([
+            'key1' => 'one',
+            'key2' => 'two',
+            'key3' => 3,
+            'key4' => null,
+            'key5' => ['test', 'ok']
+        ], $viewModel->getVariables());
+    }
+}
