371674b9d081539e44991343dc7433a7b9da2cd4
jongpak <jplabs@canxan.com>
2016-08-25 10:49:56 +0900

    Rename main.tiwg to welcome.twig

--- app/Home.php
+++ app/Home.php
@@ -10,6 +10,6 @@ class Home
     public function index(ViewModel $viewModel)
     {
         $viewModel->set('time', date('r'));
-        return 'default/main';
+        return 'default/welcome';
     }
 }
