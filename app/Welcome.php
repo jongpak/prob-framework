42d3a2be1ebde3529a54f77f3caa3a58bc61867d
jongpak <jplabs@canxan.com>
2016-09-05 00:40:26 +0900

    [Issue #12] Add login information in welcome page

--- app/Welcome.php
+++ app/Welcome.php
@@ -3,13 +3,22 @@
 namespace App\Controller;
 
 use Core\ViewModel;
+use App\Auth\AuthManager;
 
 class Welcome
 {
 
     public function index(ViewModel $viewModel)
     {
+        $accountManager = AuthManager::getInstance()->getDefaultAccountManager();
+        $loginManager = AuthManager::getInstance()->getDefaultLoginManager();
+
+        $loggedAccountId = $loginManager->getLoggedAccountId();
+
         $viewModel->set('time', date('r'));
+        $viewModel->set('accountId', $loggedAccountId);
+        $viewModel->set('accountRole', $accountManager->getRole($loggedAccountId));
+
         return 'default/welcome';
     }
 }
