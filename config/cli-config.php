ef6dce14529e443077d11c0141612d5f86d0bdef
jongpak <jplabs@canxan.com>
2016-09-08 21:04:49 +0900

    Fixed doctrine console configuration bug

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,12 +1,12 @@
 <?php
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Core\Framework;
+use Core\Application;
 
-$framework = Framework::getInstance();
-$framework->setSiteConfig(require 'site.php');
-$framework->setDbConfig(require 'db.php');
+$app = Application::getInstance();
+$app->setSiteConfig(require 'site.php');
+$app->setDbConfig(require 'db.php');
 
-$entityManager = Framework::getInstance()->getEntityManager();
+$entityManager = Application::getInstance()->getEntityManager();
 
 return ConsoleRunner::createHelperSet($entityManager);
