088d13545070bc96805ddaea8f2491dfeea3a8f6
jongpak <jplabs@canxan.com>
2016-09-09 21:49:39 +0900

    Show error message by errorReporter configuration

--- app/Bootstrap/ErrorReporterBootstrap.php
+++ app/Bootstrap/ErrorReporterBootstrap.php
@@ -9,6 +9,8 @@ class ErrorReporterBootstrap implements BootstrapInterface
 {
     public function boot()
     {
+        ini_set('display_errors', $this->getConfig()['displayErrors']);
+
         $register = new ErrorReporterRegister();
         $register->setConfig($this->getConfig());
         $register->register();
