171c8e198e1296c7905df8a25fe315e19d7e49d1
jongpak <jplabs@canxan.com>
2016-09-10 10:43:56 +0900

    Add EventManager unit test code

---
+++ tests/EventManagerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+use PHPUnit\Framework\TestCase;
+use Prob\Handler\ParameterMap;
+use JBZoo\Event\EventManager as ZooEventManager;
+
+class EventManagerTest extends TestCase
+{
+    public function testGetEventManager()
+    {
+        $this->assertEquals(ZooEventManager::class, get_class(EventManager::getEventManager()));
+    }
+
+    public function testRegisterListener()
+    {
+        EventManager::registerListener([
+            'test.event' => function () {
+                echo 'test!';
+            }
+        ]);
+
+        $this->expectOutputString('test!');
+        EventManager::getEventManager()->trigger('test.event', [new ParameterMap]);
+    }
+}
