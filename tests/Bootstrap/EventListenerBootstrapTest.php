3a838f69ae4651c90912184e1ae8fda77d26b9d4
jongpak <jplabs@canxan.com>
2016-09-10 11:37:11 +0900

    EventListenerBootstrap 테스트 코드 작성

---
+++ tests/Bootstrap/EventListenerBootstrapTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Bootstrap\Test;
+
+use PHPUnit\Framework\TestCase;
+use Core\Bootstrap\Bootstrap;
+use Core\Event\EventManager;
+use Prob\Handler\ParameterMap;
+
+class EventListenerBootstrapTest extends TestCase
+{
+
+    public function testBoot()
+    {
+        $bootstrap = new Bootstrap([
+            'App\\Bootstrap\\EventListenerBootstrap',
+        ]);
+        $bootstrap->boot([
+            'event' => [
+                'test' => function () {
+                    echo 'foo';
+                }
+            ]
+        ]);
+
+        $this->expectOutputString('foo');
+        EventManager::getEventManager()->trigger('test', [new ParameterMap()]);
+    }
+}
