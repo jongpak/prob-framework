fb3b937767944959a88a4bc5c8dd8ab6babacca5
jongpak <jplabs@canxan.com>
2016-09-10 11:37:11 +0900

    DatabaseBootstrap 테스트 코드 추가

---
+++ tests/Bootstrap/DatabaseBootstrapTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Bootstrap\Test;
+
+use PHPUnit\Framework\TestCase;
+use Core\Bootstrap\Bootstrap;
+use Core\DatabaseManager;
+use Doctrine\ORM\EntityManager;
+
+class DatabaseBootstrapTeest extends TestCase
+{
+    public function testBoot()
+    {
+        $bootstrap = new Bootstrap([
+            'App\\Bootstrap\\DatabaseBootstrap',
+        ]);
+        $bootstrap->boot([
+            'db' => [
+                'defaultConnection' => 'test',
+                'entityPath'        => [ null ],
+                'devMode'           => true,
+                'connections' => [
+                    'test' => [ 'driver'    => 'pdo_mysql' ]
+                ]
+            ]
+        ]);
+
+        $this->assertEquals(EntityManager::class, get_class(DatabaseManager::getDefaultEntityManager()));
+    }
+}
