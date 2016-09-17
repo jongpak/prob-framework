037ba8073874e06ec9d11ba9f14e903153473253
jongpak <jplabs@canxan.com>
2016-09-17 18:26:36 +0900

    Add phpDocumentor comment

--- app/Auth/AccountManagerInterface.php
+++ app/Auth/AccountManagerInterface.php
@@ -6,8 +6,22 @@ interface AccountManagerInterface
 {
     public function __construct($settings = []);
 
+    /**
+     * @param  string  $accountId
+     * @return boolean
+     */
     public function isExistAccountId($accountId);
+
+    /**
+     * @param  string  $accountId
+     * @param  string  $password
+     * @return boolean
+     */
     public function isEqualPassword($accountId, $password);
 
+    /**
+     * @param  string  $accountId
+     * @return array|null
+     */
     public function getRole($accountId);
 }
