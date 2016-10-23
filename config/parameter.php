edccfca82ceaf45dce62cf0535eed8a9bd202842
jongpak <jplabs@canxan.com>
2016-10-24 00:32:00 +0900

    [Issue #42] Add a omitted configuration of parameter wiring 'paredBody'

--- config/parameter.php
+++ config/parameter.php
@@ -11,6 +11,7 @@ use App\ViewResolver\ResponseResolver;
 use Doctrine\ORM\EntityManagerInterface;
 use Core\DatabaseManager;
 use Core\Utils\ResponseProxy;
+use Zend\Diactoros\ServerRequestFactory;
 
 return [
     P::type(ServerRequestInterface::class)
@@ -21,6 +22,9 @@ return [
 
     P::name('urlPattern')
         ->lazy(function() { return RequestMatcher::getPatternedUrl(); }),
+
+    P::name('parsedBody')
+        ->value(ServerRequestFactory::fromGlobals()->getParsedBody()),
 
     P::typeAndName('array', 'urlVariable')
         ->lazy(function() { return RequestMatcher::getUrlNameMatching(); }),
