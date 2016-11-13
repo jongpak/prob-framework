abbfaf858a1f217e30fc3f1f575891c6f0e3284d
jongpak <jplabs@canxan.com>
2016-11-13 23:30:27 +0900

    Add a unit test of LazyCallbackWiring

--- tests/ParameterWireTest.php
+++ tests/ParameterWireTest.php
@@ -2,8 +2,11 @@
 
 namespace Core;
 
+use Core\Utils\Parameter\Parameter;
 use PHPUnit\Framework\TestCase;
 use Prob\Handler\Parameter\Named;
+use Prob\Handler\Parameter\Typed;
+use Prob\Handler\Parameter\TypedAndNamed;
 use Prob\Handler\ParameterMap;
 use Prob\Handler\Proc\ClosureProc;
 
@@ -14,13 +17,53 @@ class ParameterWireTest extends TestCase
         $map = new ParameterMap();
 
         ParameterWire::appendParameter(new Named('test'), 'hello!');
+        ParameterWire::appendParameter(new Typed('array'), ['hello', 'world']);
+        ParameterWire::appendParameter(new TypedAndNamed('array', 'namedArray'), ['php']);
         ParameterWire::injectParameter($map);
 
-        $proc = new ClosureProc(function($test) {
-            return $test;
+        $proc = new ClosureProc(function($test, array $dummy, array $namedArray) {
+            return [$test, $dummy, $namedArray];
         });
 
-        $this->assertEquals('hello!', $proc->execWithParameterMap($map));
+        $this->assertEquals(['hello!', ['hello', 'world'], ['php']], $proc->execWithParameterMap($map));
+    }
+
+    public function testLazyCallbackFactory()
+    {
+        $callback = ParameterWire::lazyCallback(function() {
+            return 'test';
+        });
+
+        $this->assertEquals(true, $callback instanceof LazyWiringParameterCallback);
+        $this->assertEquals('test', $callback->exec());
+    }
+
+    public function testLazyParameterFactory()
+    {
+        $param = ParameterWire::lazy(function() {
+            return 'test';
+        });
+
+        $this->assertEquals(true, $param instanceof LazyWiringParameter);
+        $this->assertEquals('test', $param->exec());
+    }
+
+    public function testLazyCallbackWiring()
+    {
+        $map = new ParameterMap();
+
+        ParameterWire::appendParameterCallback(ParameterWire::lazyCallback(function() {
+            return [
+                Parameter::name('name')->value('name!'),
+            ];
+        }));
+        ParameterWire::injectParameter($map);
+
+        $proc = new ClosureProc(function($name) {
+            return $name;
+        });
+
+        $this->assertEquals('name!', $proc->execWithParameterMap($map));
     }
 
     public function testLazyWiring()
