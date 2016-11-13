4cf80812c6652135872da5bc7bbde2b767913e96
jongpak <jplabs@canxan.com>
2016-11-13 23:50:18 +0900

    Add a test case of lazyCallbackWiring in ParameterBootstrap

--- tests/Bootstrap/ParameterWiringBootstrapTest.php
+++ tests/Bootstrap/ParameterWiringBootstrapTest.php
@@ -3,6 +3,7 @@
 namespace App\Bootstrap\Test;
 
 use Core\ParameterWire;
+use Core\Utils\Parameter\Parameter;
 use PHPUnit\Framework\TestCase;
 use Core\Bootstrap\Bootstrap;
 use Prob\Handler\Parameter\Named;
@@ -21,17 +22,25 @@ class ParameterWiringBootstrap extends TestCase
                 [
                     'key' => new Named('test'),
                     'value' => 'hello!'
-                ]
+                ],
+                ParameterWire::lazyCallback(function() {
+                    return [
+                        [
+                            'key' => new Named('test2'),
+                            'value' => 'world!'
+                        ]
+                    ];
+                })
             ]
         ]);
 
         $map = new ParameterMap();
         ParameterWire::injectParameter($map);
 
-        $proc = new ClosureProc(function($test) {
-            return $test;
+        $proc = new ClosureProc(function($test, $test2) {
+            return $test.$test2;
         });
 
-        $this->assertEquals('hello!', $proc->execWithParameterMap($map));
+        $this->assertEquals('hello!world!', $proc->execWithParameterMap($map));
     }
 }
