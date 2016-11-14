4156cde5d0ce87d1a6557766f6f42d36fe5182e4
jongpak <jplabs@canxan.com>
2016-11-15 00:24:18 +0900

    Add TwigView unit test of custom functions

--- tests/ViewEngineTest/TwigViewTest.php
+++ tests/ViewEngineTest/TwigViewTest.php
@@ -44,4 +44,36 @@ class TwigViewTest extends TestCase
             "test.twig",
                 $view->render());
     }
+
+    public function testCssFunction()
+    {
+        $view = new TwigView($this->getTwigSetting());
+        $this->assertEquals(
+            '<link rel="stylesheet" type="text/css" href="style.css">',
+            $view->cssFunction('style.css')
+        );
+    }
+
+    public function testAssetFunction()
+    {
+        $view = new TwigView($this->getTwigSetting());
+        $this->assertEquals('/public/style.css', $view->assetFunction('style.css'));
+    }
+
+    public function testUrlFunction()
+    {
+        $view = new TwigView($this->getTwigSetting());
+        $this->assertEquals('/test/url', $view->urlFunction('/test/url'));
+    }
+
+    public function testFileFunction()
+    {
+        $view = new TwigView($this->getTwigSetting());
+
+        $view->file('/path/test');
+        $this->assertEquals('/path/test.twig', $view->fileFunction('test'));
+
+        $view->file('test');
+        $this->assertEquals('test.twig', $view->fileFunction('test'));
+    }
 }
