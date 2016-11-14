c53a47b998e9ea1449c30e3a14e40a815370478c
jongpak <jplabs@canxan.com>
2016-11-15 00:23:31 +0900

    Add dispatcher test case of DispatchWithParameter and RouteNotFoundException

--- tests/ControllerDispatcher/DispatcherTest.php
+++ tests/ControllerDispatcher/DispatcherTest.php
@@ -3,6 +3,10 @@
 namespace Core\ControllerDispatcher;
 
 use PHPUnit\Framework\TestCase;
+use Prob\Handler\Parameter\Named;
+use Prob\Handler\ParameterMap;
+use Prob\Router\Exception\RoutePathNotFound;
+use Prob\Router\Map;
 use Zend\Diactoros\ServerRequest;
 
 class DispatcherTest extends TestCase
@@ -12,7 +16,6 @@ class DispatcherTest extends TestCase
         $request = new ServerRequest([], [], '/test');
         $routerMap = (new RouterMapBuilder([
             'namespace' => null,
-
             '/test' => function () {
                 return 'test!!';
             }
@@ -26,5 +29,45 @@ class DispatcherTest extends TestCase
         $dispatcher->setRouterMap($routerMap);
 
         $this->assertEquals('test!!', $dispatcher->dispatch());
+    }
+
+    public function testDispatchWithParameter()
+    {
+        $request = new ServerRequest([], [], '/test');
+        $routerMap = (new RouterMapBuilder([
+            'namespace' => null,
+            '/test' => function ($arg) {
+                return $arg;
+            }
+        ]))->build();
+        $parameterMap = new ParameterMap();
+        $parameterMap->bindBy(new Named('arg'), 'test!!');
+
+        RequestMatcher::setRequest($request);
+        RequestMatcher::setRouterMap($routerMap);
+
+        $dispatcher = new Dispatcher();
+        $dispatcher->setRequest($request);
+        $dispatcher->setRouterMap($routerMap);
+        $dispatcher->setParameterMap($parameterMap);
+
+        $this->assertEquals('test!!', $dispatcher->dispatch());
+    }
+
+    public function testRouteNotExists()
+    {
+        $request = new ServerRequest([], [], '/NOT_FOUND');
+        $routerMap = (new RouterMapBuilder([
+            'namespace' => null,
+
+            '/test' => function () { }
+        ]))->build();
+
+        $dispatcher = new Dispatcher();
+        $dispatcher->setRequest($request);
+        $dispatcher->setRouterMap($routerMap);
+
+        $this->expectException(RoutePathNotFound::class);
+        $dispatcher->dispatch();
     }
 }
