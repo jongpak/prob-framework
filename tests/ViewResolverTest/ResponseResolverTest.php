85c04bdb4dd128ffbf63f8df12abc8bc52c93e35
jongpak <jplabs@canxan.com>
2016-11-15 22:59:15 +0900

    Add a unit test of ResponseResolverTest

---
+++ tests/ViewResolverTest/ResponseResolverTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Core\View;
+
+use App\ViewResolver\ResponseResolver;
+use PHPUnit\Framework\TestCase;
+use Zend\Diactoros\Response\EmptyResponse;
+
+class ResponseResolverTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testResponseResolve()
+    {
+        $resolver = new ResponseResolver();
+        $resolver->setViewEngineConfig([]);
+
+        $response = new EmptyResponse(200, [
+            'header-key1' => 'value1',
+            'header-key2' => 'value2',
+            'header-key3' => 'value3',
+        ]);
+
+        $responseProxy = ResponseResolver::getResponseProxyInstance();
+        $responseProxy->setResponse($response);
+
+        (new ResponseResolver())->resolve(null);
+
+        // TODO PHPUnit에서 header를 테스트하기 어렵기 때문에 일단 주석처리 함
+       /*
+        $this->assertEquals([
+            'header-key1' => 'value1',
+            'header-key2' => 'value2',
+            'header-key3' => 'value3',
+        ], headers_list());
+       */
+    }
+}
