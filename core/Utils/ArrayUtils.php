e149caa5dd88ea066bcc81ff1d28ff53629383c0
jongpak <jplabs@canxan.com>
2016-11-23 00:51:18 +0900

    ArrayUtils can find a matching string even if contain pattern characters

--- core/Utils/ArrayUtils.php
+++ core/Utils/ArrayUtils.php
@@ -16,7 +16,13 @@ class ArrayUtils
     }
 
     private static function isMatchedString($str1, $str2) {
-        return preg_match(sprintf('/^%s$/', str_replace('*', '.*', str_replace('.', '\.', $str1))), $str2)
-            || preg_match(sprintf('/^%s$/', str_replace('*', '.*', str_replace('.', '\.', $str2))), $str1);
+        $quotedStr1 = preg_quote($str1);
+        $quotedStr2 = preg_quote($str2);
+
+        $inspectStr1 = str_replace('\\*', '.*', $quotedStr1);
+        $inspectStr2 = str_replace('\\*', '.*', $quotedStr2);
+
+        return preg_match(sprintf('/^%s$/', $inspectStr1), $str2)
+            || preg_match(sprintf('/^%s$/', $inspectStr2), $str1);
     }
 }
--- tests/ArrayUtilsTest.php
+++ tests/ArrayUtilsTest.php
@@ -12,12 +12,16 @@ class ArrayUtilsTest extends TestCase
             'test.*.*',
             'test.*',
             'test',
+            'test\\one.*',
+            'test\\two.*'
         ];
 
         $this->assertEquals('test', ArrayUtils::find($arr, 'test'));
         $this->assertEquals('test.*', ArrayUtils::find($arr, 'test.some'));
         $this->assertEquals('test.*.*', ArrayUtils::find($arr, 'test.some.other'));
 
+        $this->assertEquals('test\\one.*', ArrayUtils::find($arr, 'test\\one.some'));
+        $this->assertEquals('test\\two.*', ArrayUtils::find($arr, 'test\\two.other'));
         $this->assertEquals(false, ArrayUtils::find($arr, 'not'));
         $this->assertEquals(false, ArrayUtils::find($arr, 'not.*'));
         $this->assertEquals(false, ArrayUtils::find($arr, '*.not'));
