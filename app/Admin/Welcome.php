7cb7a6d68c7ccfb9a21a4cb4e5875dfdb787bd0f
jongpak <jplabs@canxan.com>
2016-11-25 00:18:51 +0900

    Rename welcome.twig to server.twig Add url infomation in server.twig

--- app/Admin/Welcome.php
+++ app/Admin/Welcome.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use Core\Application;
 use Core\ViewModel;
 
 class Welcome {
     public function index(ViewModel $view) {
+        $view->set('siteUrl', Application::getUrl());
+        $view->set('sitePublicUrl', Application::getPublicUrl());
         $view->set('absolutePath', realpath(__DIR__ . '/../../'));
         $view->set('phpVersion', PHP_VERSION);
         $view->set('os', PHP_OS);
         $view->set('sapi', PHP_SAPI);
 
-        return 'welcome';
+        return 'server';
     }
 }
