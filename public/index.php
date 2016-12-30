aeaba818ceb8276bad4caf9c80203d54537badc3
jongpak <jplabs@canxan.com>
2016-12-30 19:34:24 +0900

    [Issue #61] Rename _bootEnv.php to _webAppBootEnv.php for support console application

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ require __DIR__ . '/../vendor/autoload.php';
 use Core\Bootstrap\Bootstrap;
 
 (new Bootstrap(require __DIR__ . '/../config/bootstrap.php'))
-    ->boot(require __DIR__ . '/../config/_bootEnv.php');
+    ->boot(require __DIR__ . '/../config/_webAppBootEnv.php');
